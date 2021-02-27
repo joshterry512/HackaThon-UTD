5ebf7669137e6bf14a6bfbba66aec3c1e1149b23
angelomoncion@gmail.com <angelomoncion@gmail.com>
2021-02-27 16:35:29 -0600

    firebase

--- App.tsx
+++ App.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './screens/LoginScreen';
+import LoadingScreen from './screens/LoadingScreen';
+import DashboardScreen from './screens/DashboardScreen';
+import {createAppContainer, createSwitchNavigator } from 'react-navigation' ;
+import firebase from 'firebase' ;
+import {firebaseConfig} from './config';
+firebase.initializeApp(firebaseConfig)
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style = {{color: '#888', fontSize: 18}}>
+        Hello World
+      </Text>
     </View>
   );
+ //return <AppNavigator />;
 }
+
+const AppSwitchNavigator = createSwitchNavigator({
+  LoadingScreen:LoadingScreen,
+  LoginScreen:LoginScreen,
+  DashboardScreen:DashboardScreen
+})
+
+const AppNavigator = createAppContainer
+(AppSwitchNavigator);
 
 const styles = StyleSheet.create({
   container: {
