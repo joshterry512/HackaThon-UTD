79d4dea94ba8e6bdf334ab59883878b127f94c21
angelomoncion@gmail.com <angelomoncion@gmail.com>
2021-02-28 01:53:47 -0600

    controller, Refresh Token

--- functions/src/user/controller.js
+++ functions/src/user/controller.js
@@ -4,6 +4,9 @@ const spotifyConfig = {
     redirectUrl: "com.authdemo://oauthredirect"
 };
 
+var SpotifyApi = new SpotifyWebApi(spotifyConfig);
+
+
 async function proxySpotifyToken(_req, res){
     //Retrieve code from request
     const code = _req.body.code;
@@ -15,13 +18,34 @@ async function proxySpotifyToken(_req, res){
 
     if(refreshToken){ 
         //Refresh token is available, retrieve a new access token
-        return res.json({ todo: "Refresh accesstoken"});
+        spotifyApi.setRefreshToken(refreshToken);
+        SpotifyApi.refreshAccessToken().then(
+            (data) => {
+                data.body.refreshToken = refreshToken;
+                return res.json(data.body);
+            },
+            (error) => {
+                console.log('Could not refresh access token...', err);
+            }
+        ).catch((oError) => {
+            return res.json(oError);
+        });
     }
 
     if(code){
         //Retrieve new refresh token and access token
-        return res.json({ todo: "Get refresh token & access token"});
+        SpotifyApi.authorizationCodeGrant(code).then(
+            (data) => {
+                return res.json(data.body);
+            },
+            (err) => {
+                console.log('Something went wrong!', err);
+            }
+        ).catch((oError) => {
+            return res.json(oError);
+        });
     }
+
 
 
 }
